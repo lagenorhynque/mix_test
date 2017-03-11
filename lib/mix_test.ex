5b88a6f13434bc31e524349086b0bb4119da34d8
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-03-11 13:13:07 +0900

    initial commit

---
+++ lib/mix_test.ex
@@ -0,0 +1,18 @@
+defmodule MixTest do
+  @moduledoc """
+  Documentation for MixTest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixTest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_test_test.exs
@@ -0,0 +1,8 @@
+defmodule MixTestTest do
+  use ExUnit.Case
+  doctest MixTest
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
