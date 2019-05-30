fe859bea23523922beb39b2d4a73975e995f400d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-05-30 23:53:28 +0900

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixTest.Mixfile do
   def project do
     [app: :mix_test,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
